6caba842add32346e8a04c27635247ce8cf68239
Vladimir Pyatkov <141109125+pyatkov-vladimir@users.noreply.github.com>
2023-08-13 16:32:16 +1000

    Add files via upload

---
+++ HW1_Terminal_script.sh
@@ -0,0 +1,17 @@
+cd f1
+mkdir f2
+mkdir f3
+mkdir f4
+cd f2
+touch file1.txt
+touch file2.txt
+touch file3.txt
+touch Js1.json
+touch Js2.json
+mkdir fa2
+mkdir fa3
+mkdir fa4
+ls -la
+mv file1.txt fa3/file1.txt
+mv file2.txt fa3/file2.txt
+
